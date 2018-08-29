554a691665e868ccc9942561ff610f8015e66fc6
Rose Parker <parker.rose@ymail.com>
2018-08-29 22:21:26 +0000

    declared 3 variables, 2 of which are urls for twitch api, 1 is an arry of twitch usernames

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-$.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
-  console.log(data);
+var streamapi="https://wind-bow.glitch.me/twitch-api/streams/";
+var channelapi="https://wind-bow.glitch.me/twitch-api/channels/";
+var channels=["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohuff", "ESL_SC2", "OgamingSC2", "comster404", "brunofin"];
+
+$(function(){
+    //calling all streamcall function on every channel
+    channels.forEach(function(channel){
+        allStreamCall(channel);
+    });
 });
